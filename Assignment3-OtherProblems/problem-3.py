c9a81f6a4c7b566b01a7a24df0e2c8c4a7e26289
RogueNPC <jordanbtansay@gmail.com>
2022-05-08 10:15:28 -0700

    Added placeholders for problems 3-2 and 3-3

---
+++ Assignment3-OtherProblems/problem-3.py
@@ -0,0 +1,6 @@
+# The same as hwk problem 5-4
+"""
+Let's say a binary tree is "super balanced" if the difference 
+between the depths of any two leaf nodes is at most one.
+Write a function to check if a binary tree is "super balanced".
+"""
